c05b21b53fa393c74e97ea55005f665ca6de8c23
Marcelo Ribeiro <marceloribeiromidia@gmail.com>
2017-02-20 20:26:38 -0300

    Add file

---
+++ slides-brand.js
@@ -0,0 +1,4 @@
+var slides = [{
+  image: 'https://firebasestorage.googleapis.com/v0/b/feiradocarnaval-ddd2d.appspot.com/o/banner-escola-axe.jpeg?alt=media&token=dc6b783c-97cc-4495-81c5-6b83b9b52116',
+  link: 'http://www.escoladeaxe.com/games/axecelular/'
+}];
